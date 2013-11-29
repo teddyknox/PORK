7958d5b5e1afd915782449524ed2419def3fe898
Will Potter <willstonepotter@gmail.com>
2013-11-29 16:23:04 -0500

    implementing validation still..just need to figure out how to run the damn regression model

--- classify.py
+++ classify.py
@@ -1,22 +1,21 @@
-import csv
+import csv, copy
 import pylab as pl
 import numpy as np
 from sklearn import datasets, linear_model, cross_validation
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-MAX_FEATURES = 100
-train_labs = []
-train_bods = []
+MAX_SIZE = 100
+total_labs = []
+total_bods = []
 
 # vectorizer = TfidfVectorizer(stop_words='english', max_features=10000, analyzer='word', ngram_range=(1, 3), token_pattern=ur'\b\w+\b', min_df=1)
 vectorizer = CountVectorizer(stop_words='english', max_features=10000, analyzer='word', ngram_range=(1,3), token_pattern=ur'\b\w+\b', min_df=1)
 
-i = 0
 
 dataset_size = 132307
 
 # cross validation by array indicies
-rs = cross_validation.ShuffleSplit(dataset_size, n_iter=2, test_size=.9)
+rs = cross_validation.ShuffleSplit(dataset_size / 10, n_iter=2, test_size=.9)
 train_indicies = []
 test_indicies = []
 
@@ -28,32 +27,47 @@ for train_index, test_index in rs:
   test_indicies.append(test_index)
 
 
-for row in train_indicies:
-  print row
-
-for row in test_indicies:
-  print row
-
 
 print "Building Dictionaries"
 with open('reddit.csv', 'rb') as csvfile:
-  spamreader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-  row_count = sum(1 for row in spamreader)
-  print row_count
-  for row in spamreader:
-    i+=1
-    if i < MAX_FEATURES:
-      train_labs.append(int(row["number_of_upvotes"]) - int(row["number_of_downvotes"]))
-      train_bods.append(row["title"])
-      # print i, row["number_of_upvotes"], row["number_of_downvotes"], int(row["number_of_upvotes"]) - int(row["number_of_downvotes"])
-      # print ', '.join(row)
-      # 
+  filereader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+  i = 0
+  for row in filereader:
+    i += 1
+    total_labs.append(int(row["number_of_upvotes"]) - int(row["number_of_downvotes"]))
+    total_bods.append(copy.deepcopy(row["title"]))
 
-print "Vectorizing"
-vectorizer.fit(train_bods)
-train_vecs = vectorizer.transform(train_bods)
-train_vecs = train_vecs.toarray()
-m = linear_model.LinearRegression()
-print "Fitting"
-m.fit(train_vecs, train_labs)
+correct = 0.0
+total = 0.0
 
+# Run for n times cross validatation
+for i in range(0, len(train_indicies)):
+  # Train indices
+  train_row = train_indicies[i]
+  # Test indices
+  test_row = test_indicies[i]
+  train_labs = []
+  train_bods = []
+  test_bods = []
+  test_labs = []
+  for i in train_row:
+    train_labs.append(total_labs[i])
+    train_bods.append(total_bods[i])
+  for i in test_row:
+    test_labs.append(total_labs[i])
+    test_bods.append(total_bods[i])
+
+  # Fit training/testing vectors
+  print "Vectorizing"
+  vectorizer.fit(total_bods)
+  train_vecs = vectorizer.transform(train_bods)
+  train_vecs = train_vecs.toarray()
+  test_vecs = vectorizer.transform(train_bods)
+  test_vecs = test_vecs.toarray()
+
+  m = linear_model.LinearRegression()
+  print "Fitting"
+  m.fit(train_vecs, train_labs)
+
+
+  print results
