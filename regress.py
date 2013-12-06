6c9db455448a36e9917791dddb4d778c44e6e509
Will Potter <willstonepotter@gmail.com>
2013-12-05 23:19:06 -0500

    now calculating stats about the dataset

--- regress.py
+++ regress.py
@@ -1,9 +1,12 @@
 import csv, copy
 import numpy as np
 import pickle
+import random
 import time
 from sklearn import cross_validation
-from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.svm import SVR
+from sklearn.feature_selection import RFECV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
 from sklearn.linear_model import LogisticRegression
 import argparse
 
@@ -11,12 +14,12 @@ class Model(object):
     def __init__(self, filename=None, num_examples=None, force_train=False):
         self.filename = filename
         self.num_examples = num_examples
-
         # short-circuit conditional
         if force_train or not self.unpickle(): # if we don't want to retrain and we have nothing to load
-            self.reg = LogisticRegression(penalty='l2')
-            self.vectorizer = HashingVectorizer(
-                ngram_range=(1,3))
+            self.reg = SVR(kernel='linear', C=1e3, gamma=0.1)
+            # self.reg = LogisticRegression(penalty='l2')
+            # self.vectorizer = CountVectorizer(ngram_range=(1,3))
+            self.vectorizer = TfidfVectorizer(norm='l2', smooth_idf=True, sublinear_tf=True, use_idf=True)
             if filename:
                 self.train(filename, num_examples=num_examples)
                 self.trained = True
@@ -88,7 +91,7 @@ class Model(object):
         results = cross_validation.cross_val_score(self.reg, data, target, cv=cv, scoring='r2', n_jobs=4)
         print results
 
-    def load_reddit_csv(self, filename, num_examples=None):
+    def load_reddit_csv(self, filename, num_examples=None, split=0.8):
         data_file = csv.reader(open(filename), delimiter=',', quotechar='"')
         shape = next(data_file)
         n_examples = int(shape[0])
@@ -104,24 +107,67 @@ class Model(object):
 
         target = np.array(target_list, dtype=np.int32)
         data = self.vectorizer.fit_transform(titles).toarray()
-        print data.shape
         return data, target
+
+def calc_stats(filename, num_examples=None):
+    data_file = csv.reader(open(filename), delimiter=',', quotechar='"')
+    shape = next(data_file)
+    names = next(data_file)
+    n_examples = int(shape[0])
+    n_features = int(shape[1])
+    if num_examples:
+        n_examples = min(n_examples, int(num_examples))
+    i = 0
+    avg_length = 0
+    avg_score = 0
+    avg_scores = {}
+    avg_counts = {}
+    max_score = float("-inf")
+    min_score = float("inf")
+    for i, n in enumerate(data_file):
+        if i >= n_examples:
+            break
+        score = int(n[10])
+        length = len(n[3].split(" "))
+        if not length in avg_scores:
+            avg_scores[length] = 0
+        if not length in avg_counts:
+            avg_counts[length] = 0
+        avg_scores[length] += int(n[10])
+        avg_counts[length] += 1
+        avg_length += length
+        avg_score += score
+        if score > max_score:
+            max_score = score
+        if score < min_score:
+            min_score = score
+    print "Average Title Length:\t", float(avg_length) / n_examples
+    print "Average Score:\t\t", float(avg_score) / n_examples
+    print "Maximum Score:\t\t", float(max_score)
+    print "Minimum Score:\t\t", float(min_score)
+
+    for k in avg_scores.keys():
+        val = avg_scores[k]
+        print "Average",k,"word title score:", float(val) / avg_counts[k]
 
 def array_generator(iterator, index, n):
     i = 0
     while i < n:
         yield next(iterator)[index] # title
         i += 1
-    print i
 
 if __name__ == '__main__': # run from command line
     parser = argparse.ArgumentParser(description='Build or test a model using reddit.csv file.')
     parser.add_argument('--test', action='store_true', help="Whether to test")
     parser.add_argument('--debug', dest='num_examples', type=int, action='store', nargs='?', const='1000', help="Train on a subset of given examples")
     parser.add_argument('--force', dest='force_train', action='store_true', help="Force retrain dataset")    
+    parser.add_argument('--stats', dest='calc_stats', action='store_true', help="Force retrain dataset")    
     args = parser.parse_args()
 
-    # initialize model
-    model = Model('reddit.csv', num_examples=args.num_examples, force_train=args.force_train)    
-    if args.test:
-        model.train_test()
+    if args.calc_stats:
+        calc_stats('reddit.csv', num_examples=args.num_examples)
+    else:
+        # initialize model
+        model = Model('reddit.csv', num_examples=args.num_examples, force_train=args.force_train)    
+        if args.test:
+            model.train_test()
